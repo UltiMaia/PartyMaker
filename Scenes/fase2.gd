8f40798ad74e1c59265e3c3576a7bad92ccc717e
Lucas Barreto <lucas_barreto29@outlook.com>
2024-05-10 20:34:42 -0300

    feat: added fase 2

--- Scenes/fase2.gd
+++ Scenes/fase2.gd
@@ -1,26 +1,26 @@
 extends Node2D
 
-@onready var balloon = $Balloon
-@onready var chair = $Chair
-@onready var party_hat = $"Party Hat"
-@onready var table = $Table
-@onready var trampoline = $Trampoline
+@onready var condensed_milk = $CondensedMilk
+@onready var chocolate_powder = $"Chocolate Powder"
+@onready var butter = $Butter
+@onready var sugar = $Sugar
+@onready var powdered_milk = $"Powdered Milk"
+@onready var texture_rect = $MarginContainer/TextureRect
 
 var textureButtonList = []
-var listObjects = ["Balloon","Chair","Party Hat","Table","Trampoline"]
+var listObjects = ["Condensed Milk","Chocolate Powder","Butter","Sugar","Powdered Milk"]
 var objects = listObjects.duplicate()
 var answer = ""
 var lives = 3
 var rightAnswer = ""
-var selectedItem = 2
-
-var white_box = Control.new()
 var red = Color(1.0,0.0,0.0,1.0) 
 var green = Color(0.0,1.0,0.0,1.0)
+var selectedItem = 2
 
 func _ready():
-	textureButtonList = [balloon, chair, party_hat,table,trampoline]
+	textureButtonList = [condensed_milk,chocolate_powder,butter,sugar,powdered_milk]
 	reduceButtonsOpacity()
+	texture_rect.modulate.a = 0.5
 	randomize()
 	selectWord()
 
@@ -30,7 +30,7 @@ func selectWord():
 		$Item.text = objects[index]
 		answer = objects[index]
 	else:
-		get_tree().change_scene_to_file("res://Scenes/fase2.tscn")
+		get_tree().change_scene_to_file("res://Scenes/start_screen.tscn")
 		
 	if lives == 0:
 		get_tree().change_scene_to_file("res://Scenes/start_screen.tscn")
@@ -39,7 +39,7 @@ func checkAnswer(guess):
 	if answer == guess:
 		objects.erase(guess)
 		$Resposta.add_theme_color_override("font_color", Color.GREEN)
-		$Resposta.text = "Você acertou!"
+		$Resposta.text = "Você acertou!" 
 		rightAnswer = guess
 	else:
 		lives -= 1
@@ -48,29 +48,29 @@ func checkAnswer(guess):
 		$Resposta.text = "Você errou..."
 	_ready()
 
-func _on_balloon_pressed():
-	checkAnswer("Balloon")
-func _on_chair_pressed():
-	checkAnswer("Chair")
-func _on_party_hat_pressed():
-	checkAnswer("Party Hat")
-func _on_table_pressed():
-	checkAnswer("Table")
-func _on_trampoline_pressed():
-	checkAnswer("Trampoline")
+func _on_condensed_milk_pressed():
+	checkAnswer("Condensed Milk")
+func _on_chocolate_powder_pressed():
+	checkAnswer("Chocolate Powder")
+func _on_sugar_pressed():
+	checkAnswer("Sugar")
+func _on_powdered_milk_pressed():
+	checkAnswer("Powdered Milk")
+func _on_butter_pressed():
+	checkAnswer("Butter")
 	
 func reduceButtonsOpacity():
 	match rightAnswer:
-		"Balloon":
-			balloon.modulate.a = 0.5
-		"Chair":
-			chair.modulate.a = 0.5
-		"Party Hat":
-			party_hat.modulate.a = 0.5
-		"Table":
-			table.modulate.a = 0.5
-		"Trampoline":
-			trampoline.modulate.a = 0.5
+		"Condensed Milk":
+			condensed_milk.modulate.a = 0.5
+		"Chocolate Powder":
+			chocolate_powder.modulate.a = 0.5
+		"Butter":
+			butter.modulate.a = 0.5
+		"Sugar":
+			sugar.modulate.a = 0.5
+		"Powdered Milk":
+			powdered_milk.modulate.a = 0.5
 
 func selectItem(elementSelected: TextureButton):
 	var texture = elementSelected.name + "_selected"
@@ -79,7 +79,7 @@ func selectItem(elementSelected: TextureButton):
 
 func unselectItem(elementSelected: TextureButton):
 	var texture = elementSelected.name 
-	var texturePath = "res://Assets/Fase 1/assets-aniversario/" + texture + ".png"
+	var texturePath = "res://Assets/Fase 2/" + texture + ".png"
 	elementSelected.texture_normal = ResourceLoader.load(texturePath)
 
 func _input(ev):

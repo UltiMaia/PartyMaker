d58e881ab6e28ce2f74ffadaf509c82b4fa273e1
Lucas Barreto <lucas_barreto29@outlook.com>
2024-05-24 20:57:42 -0300

    feat: added button blink

--- Scenes/between_stages.gd
+++ Scenes/between_stages.gd
@@ -1,8 +1,11 @@
 extends CanvasLayer
 
 @onready var label = $Label
+@onready var timer_2 = $Timer2
+@onready var button = $Button
 
 func _ready():
+	button.add_theme_color_override("font_color", Color(248,192,0))
 	if GlobalTexts.currentFase == "1":
 		label.text = "Na primeira fase, o jogador coletará as\ndecorações essenciais para a festa: \nchapeuzinhos, balões, cadeiras, \ntrampolim, e as mesas."
 	
@@ -26,3 +29,12 @@ func _on_button_pressed():
 		get_tree().change_scene_to_file("res://Scenes/start_screen.tscn")
 		return
 	get_tree().change_scene_to_file("res://Scenes/fase_" + GlobalTexts.currentFase +".tscn")
+
+
+func _on_timer_2_timeout():
+	var actualColor = button.get("theme_override_colors/font_color")
+	if actualColor == Color(248,192,0, 1):
+		button.add_theme_color_override("font_color", Color(0,0,0))
+	else:
+		button.add_theme_color_override("font_color", Color(248,192,0))
+
--- Scenes/mode_selection.gd
+++ Scenes/mode_selection.gd
@@ -1,20 +1,76 @@
 extends Node2D
 
+@onready var normal = $VBoxContainer/Normal
+@onready var hard = $VBoxContainer/Hard
+@onready var timer = $Timer
+
+var buttonSelected = "NORMAL"
 
 func _ready():
-	pass
+	changeButtonStyle()
+	
 
 
 func _process(delta):
 	pass
 
+func _input(ev):
+	if Input.is_key_pressed(KEY_RIGHT):
+		if buttonSelected == "NORMAL":
+			buttonSelected = "HARD"
+		else:
+			buttonSelected = "NORMAL"
+	if Input.is_key_pressed(KEY_LEFT):
+		if buttonSelected == "NORMAL":
+			buttonSelected = "HARD"
+		else:
+			buttonSelected = "NORMAL"
+	changeButtonStyle()	
+	if Input.is_key_pressed(KEY_BACKSPACE) || Input.is_key_pressed(KEY_SPACE):
+		selectOption()
+	
+func selectOption():
+	print(buttonSelected + " selected")
+	GlobalTexts.mode = buttonSelected
+	get_tree().change_scene_to_file("res://Scenes/between_stages.tscn")
 
 func _on_button_pressed():
-	GlobalTexts.mode = "NORMAL"
-	print(GlobalTexts.mode)
+	GlobalTexts.mode = buttonSelected
 	get_tree().change_scene_to_file("res://Scenes/between_stages.tscn")
 
 func _on_button_2_pressed():
-	GlobalTexts.mode = "HARD"
-	print(GlobalTexts.mode)
+	GlobalTexts.mode = buttonSelected
 	get_tree().change_scene_to_file("res://Scenes/between_stages.tscn")
+
+func changeButtonStyle():
+	if buttonSelected == "NORMAL":
+		changeNormalColors()
+	else:
+		changeHardColors()
+		
+func changeNormalColors():
+	normal.add_theme_color_override("font_color", Color(248,192,0))
+	hard.add_theme_color_override("font_color", Color(255,255,255))
+		
+func changeHardColors():
+	hard.add_theme_color_override("font_color", Color(248,192,0))
+	normal.add_theme_color_override("font_color", Color(255,255,255))
+		
+func _on_Timer_timeout():
+	print("Função chamada a cada 0.3 segundos")
+
+
+func _on_timer_timeout():
+	if buttonSelected == "NORMAL":
+		var actualColor = normal.get("theme_override_colors/font_color")
+		if actualColor == Color(248,192,0, 1):
+			normal.add_theme_color_override("font_color", Color(0,0,0))
+		else:
+			normal.add_theme_color_override("font_color", Color(248,192,0))
+	else:
+		var actualColor = hard.get("theme_override_colors/font_color")
+		if actualColor == Color(248,192,0, 1):
+			hard.add_theme_color_override("font_color", Color(0,0,0))
+		else:
+			hard.add_theme_color_override("font_color", Color(248,192,0))
+		

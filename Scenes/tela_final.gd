56b0ca03b4fb92fd6be47dbb0509f8930cf6cfe7
UltiMaia <plucasmaiadelima@gmail.com>
2024-05-24 13:20:22 -0300

    feat: add tela_final

---
+++ Scenes/tela_final.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+
+func _input(ev):
+	if Input.is_key_pressed(KEY_BACKSPACE) || Input.is_key_pressed(KEY_SPACE) || Input.is_key_pressed(KEY_RIGHT) || Input.is_key_pressed(KEY_LEFT):
+		_on_button_pressed()
+	
+func _on_button_pressed():
+	GlobalTexts.currentFase = "1"
+	GlobalTexts.lifes = 3
+	get_tree().change_scene_to_file("res://Scenes/start_screen.tscn")
